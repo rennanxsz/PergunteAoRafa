54286f4ffdd6f6ea38e3afbde982f2aab9e3f1f0
Rennan <rennanxsz@gmail.com>
2021-07-10 23:18:57 -0300

    Final do codigo

--- app/src/main/java/com/example/pergunteaorafa/MainActivity.java
+++ app/src/main/java/com/example/pergunteaorafa/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +17,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void gerarFrase(View view){
+
+        String[] frases = {
+                "Depois a gente vê isso.",
+                "Precisa ver com o Marcos.",
+                "Já passou para o Guto?.",
+                "Não vai fazer depois do horario!"
+        };
+
+        int numero = new Random().nextInt(4);
+        TextView respostas = findViewById(R.id.textResultado);
+        respostas.setText(frases[numero]);
 
     }
 }
